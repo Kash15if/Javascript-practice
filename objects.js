5971184f4fde0e97b89cd46814c1bd18f0c9c560
Kash15if <kash15if@yahoo.com>
2022-04-22 20:41:59 +0530

    added objects common  methods

---
+++ objects.js
@@ -0,0 +1,20 @@
+const person = {
+  fname: " John",
+  lname: " Doe",
+  age: 25,
+};
+
+console.log("for loop syntax");
+console.log("syntax:- ", "for (let x in person) {txt += person[x];}");
+console.log("delete obj.property");
+console.log("Object.entries(obj). returns array of keys");
+
+console.log(
+  "Object.entries(obj). returns array of array as key pair. i.e {1: a , b: 2}-> [[1,a], [b,2]]"
+);
+
+console.log("Object.keys(obj). returns array of keys");
+
+console.log("Object.values(obj). returns array of values onlyy");
+
+console.log("Object.freeze(obj);. freezed object cannot be modified");
